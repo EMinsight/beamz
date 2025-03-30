6e9f9c175a41c4f60f43fe37eb311482f2b58a0d
Quentin Wach <quentin.wach@googlemail.com>
2025-03-30 22:33:02 +0200

    Updated version.

--- beamz/__init__.py
+++ beamz/__init__.py
@@ -2,7 +2,7 @@
 BeamZ - Create inverse designs for your photonic devices with ease and efficiency.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
 # Import main components
 from .fdtd import FDTD
