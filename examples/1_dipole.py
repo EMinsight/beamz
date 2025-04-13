dca58815036a2aa3343bd51872f675ecb1546edb
Quentin Wach <quentin.wach@googlemail.com>
2025-04-13 07:16:27 +0200

    Updated readme and examples. It's play time!

--- examples/1_dipole.py
+++ examples/1_dipole.py
@@ -1,36 +1,13 @@
-from beamz.const import *
-from beamz.design.materials import Material
-from beamz.design.structures import *
-from beamz.design.sources import PointSource
-from beamz.simulation.signals import cosine, sigmoid, plot_signal
-from beamz.simulation.meshing import RegularGrid
-from beamz.simulation.fdtd import Simulation
+from beamz import *
+import numpy as np
 
-# Define wavelength and simulation time
 WL = 1.55*µm
-FREQ = LIGHT_SPEED/WL
-# Create a 2D slice of air
+TIME = 20*WL/LIGHT_SPEED
+T = np.linspace(0, TIME, int(TIME/(0.05*WL/LIGHT_SPEED)))
 design = Design(20*µm, 20*µm, depth=None, material=Material(1.0, 1.0, 0.0))
 design.add(Rectangle(width=10*µm, height=10*µm, material=Material(4.0, 1.0)))
-design.show()
-# Set time for the simulation
-TIME = 20*WL/LIGHT_SPEED
-DT = 0.05*WL/LIGHT_SPEED
-T = np.linspace(0, TIME, int(TIME/DT))
-print(f"TIME: {TIME}, DT: {DT}, STEPS: {int(TIME/DT)}")
-# Define the signal for the first dipole
-signal_1 = sigmoid(T, min=0, max=1, duration=TIME/4, t0=TIME/16)
-signal_1 *= cosine(T, amplitude=1.0, frequency=FREQ, phase=0)
-signal_1 *= sigmoid(T, min=1, max=0, duration=TIME/4, t0=TIME - TIME/4 - TIME/16)
-plot_signal(signal_1, T)
-# Define the signal for the second dipole
-signal_2 = sigmoid(T, min=0, max=1, duration=TIME/4, t0=TIME/16)
-signal_2 *= cosine(T, amplitude=1.0, frequency=FREQ, phase=np.pi/2)
-signal_2 *= sigmoid(T, min=1, max=0, duration=TIME/4, t0=TIME - TIME/4 - TIME/16)
-plot_signal([signal_1, signal_2], T)
-# Setup the FDTD simulation, converting the design into a grid, 
-sim = Simulation(design=RegularGrid(design, resolution=WL/20), sources=PointSource(position=(100*µm, 100*µm), signal=None))
-# Show the rasterized design and sources
-sim.show()
-# Run the simulation and live preview the results
-sim.run(save=False, animate_live=True)
+signal_1 = ramped_cosine(T, amplitude=1.0, frequency=LIGHT_SPEED/WL, ramp_duration=TIME/2)
+signal_2 = ramped_cosine(T, amplitude=1.0, frequency=LIGHT_SPEED/WL, phase=np.pi/2, ramp_duration=TIME/2)
+design.add(PointSource(position=(10*µm, 10*µm), signal=signal_1))
+design.add(PointSource(position=(10*µm, 15*µm), signal=signal_2))
+FDTD(design=design, time=T, mesh="regular", resolution=WL/20).run()
