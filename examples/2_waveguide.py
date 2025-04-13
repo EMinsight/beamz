425aad680b02a447f4303b0b1d8b89dbcf7e73be
Quentin Wach <quentin.wach@googlemail.com>
2025-04-13 02:26:14 +0200

    Fixed example dimensions.

--- examples/2_waveguide.py
+++ examples/2_waveguide.py
@@ -5,19 +5,20 @@ WL = 1.55*µm # wavelength of the light
 TIME = 20*WL/LIGHT_SPEED # duration of the simulation
 T = np.linspace(0, TIME, int(TIME/(0.01*WL/LIGHT_SPEED))) # time with 200 steps
 # Define the materials used in the design
-SiN = Material(permittivity=2.5, permeability=1.0, conductivity=0.0)
-design = Design(width=6*µm, height=2.5*µm, material=Material(permittivity=1.45, permeability=1.0, conductivity=0.0))
-design.add(Rectangle(position=(0*µm, 1.05*µm), width=6*µm, height=0.4*µm, material=SiN))
-signal = ramped_cosine(T, amplitude=1.0, frequency=LIGHT_SPEED/WL*1.5, phase=0, ramp_duration=TIME/2, t_max=TIME)
-print(signal)
-source = ModeSource(design=design, start=(1*µm, 1.6*µm), end=(1*µm, 0.9*µm), wavelength=WL, signal=signal)
-source.show()
-
-design.add(source)
+SiN = Material(permittivity=3.5**2, permeability=1.0, conductivity=0.0)  # n ≈ 3.5
+SiO2 = Material(permittivity=1.45**2, permeability=1.0, conductivity=0.0)  # n ≈ 1.45
+design = Design(width=8*µm, height=4*µm, material=SiO2)
+design.add(Rectangle(position=(0*µm, 1.5*µm), width=8*µm, height=1*µm, material=SiN))
 design.show()
 
-# Run the FDTD simulation with a regular mesh of 20 nm resolution
-FDTD(design=design, time=T, mesh="regular", resolution=0.02*µm).run()
+
+signal = ramped_cosine(T, amplitude=1.0, frequency=LIGHT_SPEED/WL, phase=0, ramp_duration=TIME/2, t_max=TIME)
+source = ModeSource(design=design, start=(1.5*µm, 1.3*µm), end=(1.5*µm, 2.6*µm), wavelength=WL, signal=signal)
+source.show()
+design.add(source)
+
+# Run FDTD with finer resolution
+FDTD(design=design, time=T, mesh="regular", resolution=0.03*µm).run()
 
 
 
