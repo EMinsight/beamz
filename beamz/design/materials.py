04e91e586d200638eca279e2e437376cf93181f0
Quentin Wach <quentin.wach@googlemail.com>
2025-04-20 08:21:30 +0200

    Revert "Getting started with adjoint optimization: Adding new material type and new examples."
    This reverts commit 85819aad5a90262594f645776ecf65f4cb33c23f.

--- beamz/design/materials.py
+++ beamz/design/materials.py
@@ -5,16 +5,6 @@ class Material:
         self.permeability = permeability
         self.conductivity = conductivity
 
-class VariableMaterial:
-    def __init__(self, permittivity_min=1.0, permittivity_max=1.0, permeability_min=1.0,
-                 permeability_max=1.0, conductivity_min=0.0, conductivity_max=0.0):
-        self.permittivity_min = permittivity_min
-        self.permittivity_max = permittivity_max
-        self.permeability_min = permeability_min
-        self.permeability_max = permeability_max
-        self.conductivity_min = conductivity_min
-        self.conductivity_max = conductivity_max
-
 # CustomMedium: Medium with user-supplied permittivity distribution.
 
 # ================================
