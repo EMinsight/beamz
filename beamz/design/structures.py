c0db0367cc32bca1792fc5163c1b49de2d6d706c
Quentin Wach <quentin.wach@googlemail.com>
2025-05-26 17:19:43 +0200

    Revert "Revert "Min dels.""
    This reverts commit 6f2a6010a2918876d190a23018e7c1d568b43a2b.

--- beamz/design/structures.py
+++ beamz/design/structures.py
@@ -7,7 +7,7 @@ from beamz.design.materials import Material
 from beamz.const import µm, EPS_0, MU_0
 from beamz.design.sources import ModeSource, GaussianSource
 from beamz.design.monitors import Monitor
-from beamz.helpers import display_header, display_status, tree_view, console, get_si_scale_and_label
+from beamz.helpers import display_status, tree_view, get_si_scale_and_label
 import colorsys
 import gdspy
 
