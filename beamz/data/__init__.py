811a8f52ded1ca47bdde122c69bf5c4016255fec
Quentin Wach <quentin.wach@googlemail.com>
2026-05-23 04:36:00 +0200

    Add xarray data package dependency

---
+++ beamz/data/__init__.py
@@ -0,0 +1,19 @@
+"""Data conversion helpers for BeamZ result objects."""
+
+from beamz.data.xarray import (
+    field_data_array,
+    mode_dataset,
+    monitor_dataset,
+    simulation_dataset,
+    source_dataset,
+    source_signal_data_array,
+)
+
+__all__ = [
+    "field_data_array",
+    "mode_dataset",
+    "monitor_dataset",
+    "simulation_dataset",
+    "source_dataset",
+    "source_signal_data_array",
+]
